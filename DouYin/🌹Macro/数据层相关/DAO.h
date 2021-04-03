709a82c562c17675a762ce17aa9527f487dea0d1
Jobs <lg295060456@gmail.com>
2021-04-03 18:53:01 +0700

    no message

---
+++ DouYin/🌹Macro/数据层相关/DAO.h
@@ -0,0 +1,25 @@
+//
+//  DAO.h
+//  DouDong-II
+//
+//  Created by Jobs on 2020/12/19.
+//
+
+#ifndef DAO_h
+#define DAO_h
+/// 数据模型层
+#import "BaseModel.h"
+/// 网络请求相关的配置
+// URL_Manager
+#import "URLManagerModel.h"
+#import "NSObject+URLManager.h"
+// 存取页面数据
+#import "DataManager.h"
+// App 的所有网络请求Api
+#import "DDNetworkingAPI.h"
+// 请求的公共配置文件
+#import "RequestTool.h"
+// 用户信息
+
+
+#endif /* DAO_h */

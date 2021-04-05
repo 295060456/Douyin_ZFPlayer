6c674f4eab5050947d540445245e6823b1c51c88
Jobs <lg295060456@gmail.com>
2021-04-05 18:56:56 +0700

    no message

--- DouYin/🇨🇳ServiceLogic/Common/网络请求相关的配置/URL_Manager/NSObject+URLManager/NSObject+URLManager.m
+++ DouYin/🇨🇳ServiceLogic/Common/网络请求相关的配置/URL_Manager/NSObject+URLManager/NSObject+URLManager.m
@@ -8,18 +8,12 @@
 
 #import "NSObject+URLManager.h"
 
-/// 柬埔寨（主要）开发环境
-BOOL DevEnviron_Cambodia_Main = YES;
-///// 柬埔寨（次要）开发环境
-BOOL DevEnviron_Cambodia_Minor = NO;
-/// 柬埔寨Rally（次要）开发环境
-BOOL DevEnviron_Cambodia_Rally = NO;
-/// 中国大陆开发环境
-BOOL DevEnviron_China_Mainland = NO;
-/// 测试环境
-BOOL TestEnviron = NO;
-/// 生产环境
-BOOL ProductEnviron = NO;
+//NetworkingEnvir networkingEnvir = DevEnviron_Cambodia_Main;/// 柬埔寨（主要）开发环境
+//NetworkingEnvir networkingEnvir = DevEnviron_Cambodia_Minor;/// 柬埔寨（次要）开发环境
+//NetworkingEnvir networkingEnvir = DevEnviron_Cambodia_Rally;/// 柬埔寨Rally（次要）开发环境
+//NetworkingEnvir networkingEnvir = DevEnviron_China_Mainland;/// 中国大陆开发环境
+NetworkingEnvir networkingEnvir = TestEnviron;/// 测试环境
+//NetworkingEnvir networkingEnvir = ProductEnviron;/// 生产环境
 
 @implementation NSObject (URLManager)
 /*
@@ -56,50 +50,66 @@ BOOL ProductEnviron = NO;
 }
 #pragma mark —— BaseURL
 +(NSString *)BaseUrl{
-    if (DevEnviron_Cambodia_Main) {
-        /// 柬埔寨（主要）开发环境
-        return @"http://172.24.135.53:8011/";
-    }else if(DevEnviron_Cambodia_Minor){
-        /// 柬埔寨的开发环境(次要)，因为某些代码未合并，柬埔寨的开发环境临时用测试环境替代
-        return @"";
-    }else if (DevEnviron_Cambodia_Rally){
-        /// 柬埔寨Rally（次要）开发环境
-        return @"http://172.24.135.15:8011/";
-    }else if (DevEnviron_China_Mainland){
-        /// 中国大陆开发环境
-        return @"http://222.186.150.148:81/api/";
-    }else if (TestEnviron){
-        /// 测试环境
-        return @"http://172.24.135.54/api/";
-    }else if (ProductEnviron){
-        /// 生产环境
-        return @"https://www.xiuwa.top/api/";
-    }else{
-        return @"";
+    switch (networkingEnvir) {
+        case DevEnviron_Cambodia_Main:{
+            /// 柬埔寨（主要）开发环境
+            return @"http://172.24.135.53:8011/";
+        }break;
+        case DevEnviron_Cambodia_Minor:{
+            /// 柬埔寨的开发环境(次要)，因为某些代码未合并，柬埔寨的开发环境临时用测试环境替代
+            return @"";
+        }break;
+        case DevEnviron_Cambodia_Rally:{
+            /// 柬埔寨Rally（次要）开发环境
+            return @"http://172.24.135.15:8011/";
+        }break;
+        case DevEnviron_China_Mainland:{
+            /// 中国大陆开发环境
+            return @"http://222.186.150.148:81/api/";
+        }break;
+        case TestEnviron:{
+            /// 测试环境
+            return @"http://172.24.135.54/api/";
+        }break;
+        case ProductEnviron:{
+            /// 生产环境
+            return @"https://www.xiuwa.top/api/";
+        }break;
+        default:
+            return @"";
+            break;
     }
 }
 
 +(NSString *)BaseUrl_H5{
-    if (DevEnviron_Cambodia_Main) {
-        /// 柬埔寨（主要）开发环境
-        return @"http://222.186.150.148/taskpage";
-    }else if (DevEnviron_Cambodia_Minor){
-        /// 柬埔寨的开发环境(次要)，因为某些代码未合并，柬埔寨的开发环境临时用测试环境替代
-        return @"";
-    }else if (DevEnviron_Cambodia_Rally){
-        /// 柬埔寨Rally（次要）开发环境
-        return @"http://172.24.135.15/api/";
-    }else if (DevEnviron_China_Mainland){
-        /// 中国大陆开发环境
-        return @"http://222.186.150.148:81/taskpage-white";
-    }else if (TestEnviron){
-        /// 测试环境
-        return @"http://172.24.135.54/taskpage-white";
-    }else if (ProductEnviron){
-        /// 生产环境
-        return @"https://www.xunniao.top/h5-white";
-    }else{
-        return @"";
+    switch (networkingEnvir) {
+        case DevEnviron_Cambodia_Main:{
+            /// 柬埔寨（主要）开发环境
+            return @"http://222.186.150.148/taskpage";
+        }break;
+        case DevEnviron_Cambodia_Minor:{
+            /// 柬埔寨的开发环境(次要)，因为某些代码未合并，柬埔寨的开发环境临时用测试环境替代
+            return @"";
+        }break;
+        case DevEnviron_Cambodia_Rally:{
+            /// 柬埔寨Rally（次要）开发环境
+            return @"http://172.24.135.15/api/";
+        }break;
+        case DevEnviron_China_Mainland:{
+            /// 中国大陆开发环境
+            return @"http://222.186.150.148:81/taskpage-white";
+        }break;
+        case TestEnviron:{
+            /// 测试环境
+            return @"http://172.24.135.54/taskpage-white";
+        }break;
+        case ProductEnviron:{
+            /// 生产环境
+            return @"https://www.xunniao.top/h5-white";
+        }break;
+        default:
+            return @"";
+            break;
     }
 }
 #pragma mark —— 社区相关接口
